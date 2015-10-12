8c45f6427212f01345379c8fcff1fa7ba4dc0312
Anders Magnusson <ragge@tethuvudet.se>
2015-10-12 11:29:34 +0000

    Add basic tests for PIC compiled code.

--- regress/misc/shlib/lib.c
+++ regress/misc/shlib/lib.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-#if defined(__PCC__)
-#define __constructor _Pragma("constructor")
-#define __destructor _Pragma("destructor")
-#elif defined(__GNUC__)
+#if defined(__GNUC__) || defined(__PCC__)
 #define __constructor __attribute__((constructor))
 #define __destructor __attribute__((destructor))
 #endif
